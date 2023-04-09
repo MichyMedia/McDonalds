67bc7e10f87e7ecd826d4ac9ab4fc1d20fcffdeb
MichyMedia <96814843+MichyMedia@users.noreply.github.com>
2023-04-09 14:40:45 +1000

    Create config.lua

---
+++ qb-smallresources/config.lua
@@ -0,0 +1,38 @@
+Config.ConsumablesEat = {
+
+    ["bigmac"] = math.random(15, 20),
+    ["mcchicken"] = math.random(15, 20),--
+    ["quarterpounder"] = math.random(15, 20),
+    ["dquarterpounder"] = math.random(15, 20),--
+    ["filet-o-fish"] = math.random(15, 20),--
+    ["cheeseburger"] = math.random(15, 20),--
+    ["bigmac_meal"] = math.random(25, 40),--
+    ["mcchicken_meal"] = math.random(25, 40),
+    ["quarterpounder_meal"] = math.random(25, 40),
+    ["dquarterpounder_meal"] = math.random(25, 40),
+    ["filet-o-fish_meal"] = math.random(25, 40),
+    ["cheeseburger_meal"] = math.random(25, 40),
+    ["dcheeseburger"] = math.random(15, 20),
+    ["tcheeseburger"] = math.random(15, 20),
+    ["6pnuggets"] = math.random(15, 20),
+    ["10pnuggets"] = math.random(15, 20),
+    ["20pnuggets"] = math.random(15, 20),
+    ["dcheeseburger_meal"] = math.random(25, 40),
+    ["tcheeseburger_meal"] = math.random(25, 40),
+    ["6pnuggets_meal"] = math.random(25, 40),
+    ["10pnuggets_meal"] = math.random(25, 40),
+    ["20pnuggets_meal"] = math.random(25, 40),
+    ["sfries"] = math.random(5, 10),
+    ["mfries"] = math.random(15, 20),
+    ["lfries"] = math.random(20, 25),
+    ["hotapplepie"] = math.random(15, 20),
+}
+
+Config.ConsumablesDrink = {
+    ["coffee"] = math.random(40, 50),
+    ["ssconef"] = math.random(15, 20),
+    ["sscone"] = math.random(15, 20),
+    ["mflurryo"] = math.random(15, 20),
+    ["mflurrym"] = math.random(15, 20),
+    ["cocacola"] = math.random(20, 30),
+}
